46094cf64c65d584facbc69fd5d21a17688bf843
marqu <marqu@DESKTOP-N339GVH>
2025-04-29 20:54:49 -0400

    Se agrego el Ejercicio 6

---
+++ Ejercicio 6_Serie fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0;
+        int b = 1;
+
+        Console.WriteLine("Serie Fibonacci del 1 al 50:");
+
+        while (b <= 50)
+        {
+            if (b >= 1)
+            {
+                Console.WriteLine(b);
+            }
+
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
