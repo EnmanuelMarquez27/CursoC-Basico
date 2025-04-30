ba7de062153a855ea460283005e2d54e1d2f48d5
marqu <marqu@DESKTOP-N339GVH>
2025-04-29 20:48:50 -0400

    Se agregaron los ejercicios 4 y 5

---
+++ Ejercicio 4_Numero primo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero positivo: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero <= 1)
+        {
+            Console.WriteLine("El número NO es primo.");
+        }
+        else
+        {
+            bool esPrimo = true;
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo)
+            {
+                Console.WriteLine("El número SÍ es primo.");
+            }
+            else
+            {
+                Console.WriteLine("El número NO es primo.");
+            }
+        }
+
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
---
+++ Ejercicio 5_Numeros Impares/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números impares del 1 al 30:");
+
+        for (int i = 1; i <= 30; i++)
+        {
+            if (i % 2 != 0) // si el residuo de dividir entre 2 no es 0, es impar
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
